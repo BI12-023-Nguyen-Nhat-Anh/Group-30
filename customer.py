80da1e6fc4fc3aa700011acf55cdc3d5da622b47
nanh123 <anhnn.bi12-023@st.usth.edu.vn>
2023-03-24 17:31:02 +0700

    add function set_tax

--- customer.py
+++ customer.py
@@ -1,13 +1,9 @@
 import re # The re library provides tools to use regular expressions in Python
 
 class person:
-    def __init__(self,id,name,tax):
+    def __init__(self,id,name):
         self.__id=id
         self.__name=name
-        if not re.match(r"^[0-9]{10}$", tax):   # Check if tax number have the right formula 10 digits contain from 0-9 
-            self.__tax=False
-        else:
-            self.__tax=tax
     
     def get_name(self):
         return self.__name
@@ -19,20 +15,17 @@ class person:
         return self.__tax
 
 class customer(person):
-    def __init__(self,id,name,address,phone_number,email,tax,type):
-        super().__init__(id,name,tax)
+    def __init__(self,id,name,address,phone_number,email,type):
+        super().__init__(id,name)
         self.__address=address
-
         if not re.match(r"^[0-9]{10}$", phone_number):    # Similar to tax number check if phone_number has the right formula
             self.__phone_number=False
         else:
             self.__phone_number=phone_number
-
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email):    # Similar to tax number check the formula of email
             self.__email=False
         else:
             self.__email=email
-
         self.__type=type
 
     def get_address(self):
@@ -46,3 +39,10 @@ class customer(person):
     
     def get_type(self):
         return self.__type
+    
+    if(get_type()>1):
+        def set_tax(self,tax):
+            if not re.match(r"^[0-9]{10}$", tax):   # Check if tax number have the right formula 10 digits contain from 0-9 
+                self.__tax=False
+            else:
+                self.__tax=tax
