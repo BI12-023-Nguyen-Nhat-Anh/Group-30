348c326f241bf068b171cb3b31b5fbb548cc32bb
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-03-30 15:36:37 +0700

    Register

--- GUI/register.py
+++ GUI/register.py
@@ -22,7 +22,8 @@ Mylabel12 = Label(window,text="Tax identification numbers",width=25,bg="#fff").p
 
 MyEntry = Entry(window,borderwidth=5,font=25).place(relx=0.02,rely=0.13,relwidth = 0.45,relheight = 0.06)
 MyEntry1 = Entry(window,borderwidth=5,font=25).place(relx=0.5,rely=0.13,relwidth = 0.45,relheight = 0.06)
-MyEntry2 = Entry(window,borderwidth=5,font=25).place(relx=0.02,rely=0.35,relwidth = 0.3,relheight = 0.06)
+MyEntry2 = Entry(window,borderwidth=5,font=25)
+MyEntry2.place(relx=0.02,rely=0.35,relwidth = 0.3,relheight = 0.06)
 MyEntry3 = Entry(window,borderwidth=5,font=25).place(relx=0.35,rely=0.35,relwidth = 0.3,relheight = 0.06)
 MyEntry4 = Entry(window,borderwidth=5,font=25).place(relx=0.68,rely=0.35,relwidth = 0.3,relheight = 0.06)
 MyEntry5 = Entry(window,borderwidth=5,font=25).place(relx=0.02,rely=0.53,relwidth = 0.45,relheight = 0.06)
@@ -32,57 +33,61 @@ MyEntry8 = Entry(window,borderwidth=5,font=25).place(relx=0.515,rely=0.73,relwid
 
 def onClick_Province():
     def Close_tab():
-        myLable.destroy()
+        myFrame.destroy()
         MyButton.config(image = my_img)
         MyButton.config(command = onClick_Province)
-    myLable = Label(window, text=" ",borderwidth=5,font=25,bg="black")
-    myLable.place(relx=0.02,rely=0.42,relwidth = 0.3,relheight = 0.06)
-    MyButton.config(text = 'X')
-    MyButton.config(command = Close_tab)
-    my_img = ImageTk.PhotoImage(file="assets/Arrow_Down.png")
+    
+    myFrame = Frame(window,bg="black")
+    myFrame.place(relx=0.02,rely=0.42,height= 250,relwidth= 0.26)
+    
+    hanoi_btn = Button(myFrame,text= "Hanoi", font=('Bold',20),fg='white',bg="black",command=lambda: MyEntry2.insert(0, hanoi_btn["text"]))
+    hanoi_btn.place(relx=0.01,rely=0.1,relwidth=0.98,relheight=0.2)
 
+    MyButton.config(command = Close_tab)
+    
 def onClick_District():
     def Close_tab():
-        myLable.destroy()
-        MyButton.config(image = my_img)
-        MyButton.config(command = onClick_District)
-    myLable = Label(window, text=" ",borderwidth=5,font=25,bg="black")
-    myLable.place(relx=0.35,rely=0.42,relwidth = 0.3,relheight = 0.06)
-    MyButton1.config(text = 'X')
+        myFrame1.destroy()
+        MyButton1.config(image = my_img)
+        MyButton1.config(command = onClick_District)
+    
+    myFrame1 = Frame(window,bg="black")
+    myFrame1.place(relx=0.35,rely=0.42,height= 250,relwidth= 0.26)
+    
     MyButton1.config(command = Close_tab)
-    my_img = ImageTk.PhotoImage(Image.open("assets/Arrow_Down.png"))
 
 def onClick_Ward():
     def Close_tab():
-        myLable.destroy()
-        MyButton.config(image = my_img)
-        MyButton.config(command = onClick_Ward)
-    myLable = Label(window, text=" ",borderwidth=5,font=25,bg="black")
-    myLable.place(relx=0.68,rely=0.42,relwidth = 0.3,relheight = 0.06)
-    MyButton2.config(text = 'X')
+        myFrame2.destroy()
+        MyButton2.config(image = my_img)
+        MyButton2.config(command = onClick_Ward)
+        
+    myFrame2 = Frame(window,bg="black")
+    myFrame2.place(relx=0.68,rely=0.42,height= 250,relwidth= 0.26)
+    
     MyButton2.config(command = Close_tab)
-    my_img = ImageTk.PhotoImage(Image.open("assets/Arrow_Down.png"))
 
 def onClick_Intended():
     def Close_tab():
-        myLable.destroy()
-        MyButton.config(image = my_img)
-        MyButton.config(command = onClick_Intended)
-    myLable =Label(window, text=" ",borderwidth=5,font=25,bg="black")
-    myLable.place(relx=0.02,rely=0.8,relwidth = 0.45,relheight = 0.06)
-    MyButton3.config(text = 'X')
+        myFrame3.destroy()
+        MyButton3.config(image = my_img)
+        MyButton3.config(command = onClick_Intended)
+        
+    
+    myFrame3 =Frame(window,bg="black")
+    myFrame3.place(relx=0.02,rely=0.8,height= 250,relwidth= 0.40)
+    
     MyButton3.config(command = Close_tab)
-    my_img = ImageTk.PhotoImage(Image.open("assets/Arrow_Down.png"))
 
-my_img = ImageTk.PhotoImage(Image.open("assets/Arrow_Down.png"))
-MyButton = Button(image = my_img,bg="#fff",borderwidth=5,activebackground="#158aff",activeforeground="white",command=onClick_Province)
-MyButton.place(relx = 0.28,rely = 0.35, relwidth = 0.04, relheight = 0.056)
-MyButton1 = Button(image= my_img,bg="#fff",borderwidth=5,activebackground="#158aff",activeforeground="white",command=onClick_District)
-MyButton1.place(relx = 0.62,rely = 0.35, relwidth = 0.04, relheight = 0.056)
-MyButton2 = Button(image= my_img,bg="#fff",borderwidth=5,activebackground="#158aff",activeforeground="white",command=onClick_Ward)
-MyButton2.place(relx = 0.94,rely = 0.35, relwidth = 0.04, relheight = 0.056)
-MyButton3 = Button(image= my_img,bg="#fff",borderwidth=5,activebackground="#158aff",activeforeground="white",command=onClick_Intended)
-MyButton3.place(relx = 0.43,rely = 0.73, relwidth = 0.04, relheight = 0.056)
+my_img = ImageTk.PhotoImage(Image.open("assets/Arrow_1.png"))
+MyButton = Button(image = my_img,bg="#fff",bd=0,activebackground="#fff",activeforeground="white",command=onClick_Province)
+MyButton.place(relx = 0.275,rely = 0.36, relwidth = 0.04, relheight = 0.036)
+MyButton1 = Button(image= my_img,bg="#fff",bd=0,activebackground="#fff",activeforeground="white",command=onClick_District)
+MyButton1.place(relx = 0.605,rely = 0.36, relwidth = 0.04, relheight = 0.036)
+MyButton2 = Button(image= my_img,bg="#fff",bd=0,activebackground="#fff",activeforeground="white",command=onClick_Ward)
+MyButton2.place(relx = 0.935,rely = 0.36, relwidth = 0.04, relheight = 0.036)
+MyButton3 = Button(image= my_img,bg="#fff",bd=0,activebackground="#fff",activeforeground="white",command=onClick_Intended)
+MyButton3.place(relx = 0.425,rely = 0.74, relwidth = 0.04, relheight = 0.036)
 
 
 window.mainloop()
