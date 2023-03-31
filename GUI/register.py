d5f2f75dd93fa62c4258941e8f4ca3104fee0fc3
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-03-31 21:19:59 +0700

    Regitster

--- GUI/register.py
+++ GUI/register.py
@@ -72,6 +72,39 @@ MyEntry7.place(relx=0.02, rely=0.73, relwidth=0.45, relheight=0.06)
 MyEntry8 = Entry(window, borderwidth=5, font=25)
 MyEntry8.place(relx=0.515, rely=0.73, relwidth=0.45, relheight=0.06)
 
+def Save_Data():
+    
+    name = MyEntry.get()
+    identify = MyEntry1.get()
+    province = MyEntry2.get()
+    district = MyEntry3.get()
+    ward = MyEntry4.get()
+    electricity_usage = MyEntry5.get()
+    residental_adress = MyEntry6.get()
+    intended_use = MyEntry7.get()
+    tax = MyEntry8.get()
+    with open("data_register.txt",'w') as wf:
+        write = wf.write(f"[{name}, {identify}]: {province};{district};{ward};{electricity_usage};{residental_adress};{intended_use};{tax}")
+        if write == "":
+            write.pop()
+        
+    if (name != "") and (identify != "") and (province != "") and (district != "") and (ward != "") and (residental_adress != "") and (intended_use != "") and (tax != ""):
+        with open("register.txt", 'a') as af:
+            with open("data_register.txt",'r') as r:
+                read = r.read()
+            r.close()
+            af.write(str(read)+ "\n")
+    
+def Clear_Data():
+    MyEntry.delete(0,END)
+    MyEntry1.delete(0,END)
+    MyEntry2.delete(0,END)
+    MyEntry3.delete(0,END)
+    MyEntry4.delete(0,END)
+    MyEntry5.delete(0,END)
+    MyEntry6.delete(0,END)
+    MyEntry7.delete(0,END)
+    MyEntry8.delete(0,END)
 
 def onClick_Province():
     def Close_tab():
@@ -79,11 +112,11 @@ def onClick_Province():
         MyButton.config(image=my_img)
         MyButton.config(command=onClick_Province)
 
-    myFrame = Frame(window, bg="#fff", borderwidth=20)
-    myFrame.config( highlightbackground="black")
+    
+    myFrame = Frame(window, bg="#fff", borderwidth=2, relief="solid")
     myFrame.place(relx=0.02, rely=0.42, height=250, relwidth=0.26)
 
-    hanoi_btn = Button(myFrame, text="Hanoi", font=('Bold', 20), fg='black', bg="#fff", bd=0,
+    hanoi_btn = Button(myFrame, text="Ha Noi", font=('Bold', 20), fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
                    command=lambda: MyEntry2.insert(0, hanoi_btn["text"]))
     hanoi_btn.place(relx=0.01, rely=0.1, relwidth=0.98, relheight=0.2)
 
@@ -96,7 +129,7 @@ def onClick_District():
         MyButton1.config(image=my_img)
         MyButton1.config(command=onClick_District)
 
-    myFrame1 = Frame(window, bg="black")
+    myFrame1 = Frame(window, bg="#fff",borderwidth=2, relief="solid")
     myFrame1.place(relx=0.35, rely=0.42, height=250, relwidth=0.26)
 
     # create a scrollbar for the frame
@@ -110,32 +143,56 @@ def onClick_District():
     scrollbar.config(command=canvas.yview)
 
     # create a new frame inside the canvas to hold the buttons
-    inner_frame = Frame(canvas, bg="black")
+    inner_frame = Frame(canvas, bg="#fff")
     canvas.create_window((0, 0), window=inner_frame, anchor='nw')
 
-    bactuliem_btn = Button(inner_frame, text="BacTuLiem", font=('Bold', 20), fg='white', bg="black", bd=0,
+    bactuliem_btn = Button(inner_frame, text="Bac Tu Liem", font=('Bold', 20), fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
                            command=lambda: MyEntry3.insert(END, "BacTuLiem"))
     bactuliem_btn.pack(fill=X, padx=50, pady=5)
 
-    badinh_btn = Button(inner_frame, text="BaDinh", font=('Bold', 20), fg='white', bg="black", bd=0,
+    badinh_btn = Button(inner_frame, text="Ba Dinh", font=('Bold', 20), fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
                         command=lambda: MyEntry3.insert(END, "BaDinh"))
     badinh_btn.pack(fill=X, padx=50, pady=5)
 
-    caugiay_btn = Button(inner_frame, text="CauGiay", font=('Bold', 20), fg='white', bg="black", bd=0,
+    caugiay_btn = Button(inner_frame, text="Cau Giay", font=('Bold', 20), fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
                          command=lambda: MyEntry3.insert(END, "CauGiay"))
     caugiay_btn.pack(fill=X, padx=50, pady=5)
 
-    dongda_btn = Button(inner_frame, text="DongDa", font=('Bold', 20), fg='white', bg="black", bd=0,
+    dongda_btn = Button(inner_frame, text="Dong Da", font=('Bold', 20), fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
                         command=lambda: MyEntry3.insert(END, "DongDa"))
     dongda_btn.pack(fill=X, padx=50, pady=5)
 
-    haibatrung_btn = Button(inner_frame, text="HaiBaTrung", font=('Bold', 20), fg='white', bg="black", bd=0,
+    haibatrung_btn = Button(inner_frame, text="Hai Ba Trung", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
                             command=lambda: MyEntry3.insert(END, "HaiBaTrung"))
     haibatrung_btn.pack(fill=X, padx=50, pady=5)
 
-    hoankien_btn = Button(inner_frame, text="HoanKiem", font=('Bold', 20), fg='white', bg="black", bd=0,
+    hoankiem_btn = Button(inner_frame, text="Hoan Kiem", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
                           command=lambda: MyEntry3.insert(END, "HoanKiem"))
-    hoankien_btn.pack(fill=X, padx=50, pady=5)
+    hoankiem_btn.pack(fill=X, padx=50, pady=5)
+
+    hadong_btn = Button(inner_frame, text="Ha Dong", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry3.insert(END, "HaDong"))
+    hadong_btn.pack(fill = X, padx= 50, pady=5)
+
+    hoangmai_btn =  Button(inner_frame, text="Hoang Mai", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry3.insert(END, "HoangMai"))
+    hoangmai_btn.pack(fill=X, padx = 50,pady= 5)
+
+    longbien_btn =  Button(inner_frame, text="Long Bien", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry3.insert(END, "LongBien"))
+    longbien_btn.pack(fill=X, padx = 50,pady= 5)
+
+    thanhxuan_btn = Button(inner_frame, text="Thanh Xuan", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry3.insert(END, "ThanhXuan"))
+    thanhxuan_btn.pack(fill=X, padx = 50,pady= 5)
+
+    tayho_btn = Button(inner_frame, text="Tay Ho", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry3.insert(END, "TayHo"))
+    tayho_btn.pack(fill=X, padx = 50,pady= 5)
+
+    namtuliem_btn = Button(inner_frame, text="Nam Tu Liem", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry3.insert(END, "NamTuLiem"))
+    namtuliem_btn.pack(fill=X, padx = 50,pady= 5)
 
     # update the canvas scroll region
     inner_frame.update_idletasks()
@@ -150,9 +207,58 @@ def onClick_Ward():
         MyButton2.config(image=my_img)
         MyButton2.config(command=onClick_Ward)
 
-    myFrame2 = Frame(window, bg="black")
+    myFrame2 = Frame(window, bg="#fff",borderwidth=2, relief="solid")
     myFrame2.place(relx=0.68, rely=0.42, height=250, relwidth=0.26)
 
+    # create a scrollbar for the frame
+    scrollbar1 = Scrollbar(myFrame2)
+    scrollbar1.pack(side=RIGHT, fill=Y)
+
+    # create a canvas to hold the buttons and attach the scrollbar to it
+    canvas1 = Canvas(myFrame2, yscrollcommand=scrollbar1.set)
+    canvas1.pack(side=LEFT, fill=BOTH, expand=True)
+
+    scrollbar1.config(command=canvas1.yview)
+
+    # create a new frame inside the canvas to hold the buttons
+    inner_frame1 = Frame(canvas1, bg="#fff")
+    canvas1.create_window((0, 0), window=inner_frame1, anchor='nw')
+
+    dichvong_btn = Button(inner_frame1, text="Dich Vong", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "DichVong"))
+    dichvong_btn.pack(fill=X, padx = 50,pady= 5)
+
+    dichvonghau_btn =  Button(inner_frame1, text="Dich Vong Hau", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "DichVongHau"))
+    dichvonghau_btn.pack(fill=X, padx = 50,pady= 5)
+
+    maidich_btn = Button(inner_frame1, text="Mai Dich", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "MaiDich"))
+    maidich_btn.pack(fill=X, padx = 50,pady= 5)
+
+    nghiado_btn = Button(inner_frame1, text="Nghia Do", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "NghiaDo"))
+    nghiado_btn.pack(fill=X, padx = 50,pady= 5)
+
+    nghiatan_btn = Button(inner_frame1, text="Nghia Tan", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "NghiaTan"))
+    nghiatan_btn.pack(fill=X, padx = 50,pady= 5)
+
+    quanhoa_btn = Button(inner_frame1, text="Quan Hoa", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "QuanHoa"))
+    quanhoa_btn.pack(fill=X, padx = 50,pady= 5)
+
+    trunghoa_btn = Button(inner_frame1, text="Trung Hoa", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "TrungHoa"))
+    trunghoa_btn.pack(fill=X, padx = 50,pady= 5)
+
+    yenhoa_btn = Button(inner_frame1, text="YenHoa", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry4.insert(END, "YenHoa"))
+    yenhoa_btn.pack(fill=X, padx = 50,pady= 5)
+
+    # update the canvas scroll region
+    inner_frame1.update_idletasks()
+    canvas1.config(scrollregion=canvas1.bbox("all"))
     # update the button command to close the tab
     MyButton2.config(command=Close_tab)
 
@@ -162,9 +268,42 @@ def onClick_Intended():
         MyButton3.config(image=my_img)
         MyButton3.config(command=onClick_Intended)
 
-    myFrame3 = Frame(window, bg="black")
-    myFrame3.place(relx=0.02, rely=0.8, height=250, relwidth=0.40)
+    myFrame3 = Frame(window, bg="#fff",borderwidth=2, relief="solid")
+    myFrame3.place(relx=0.02, rely=0.8, height=100, relwidth=0.40)
 
+    # create a scrollbar for the frame
+    scrollbar = Scrollbar(myFrame3)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    # create a canvas to hold the buttons and attach the scrollbar to it
+    canvas = Canvas(myFrame3, yscrollcommand=scrollbar.set)
+    canvas.pack(side=LEFT, fill=BOTH, expand=True)
+
+    scrollbar.config(command=canvas.yview)
+
+    # create a new frame inside the canvas to hold the buttons
+    inner_frame = Frame(canvas, bg="#fff")
+    canvas.create_window((0, 0), window=inner_frame, anchor='nw')
+
+    household_btn = Button(inner_frame, text="HouseHold", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry7.insert(END, "HouseHold"))
+    household_btn.pack(fill=X, padx = 50,pady= 5)
+
+    administrativeoffices_btn = Button(inner_frame, text="Administrative Offices", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry7.insert(END, "Administrative Offices"))
+    administrativeoffices_btn.pack(fill=X, padx = 50,pady= 5)
+
+    business_btn = Button(inner_frame, text="Business", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry7.insert(END, "Business"))
+    business_btn.pack(fill=X, padx = 50,pady= 5)
+
+    manufacturingindustries_btn = Button(inner_frame, text="Manufacturing Industries", font=('Bold', 20),fg='black', bg="#fff", bd=0,activebackground="#fff",activeforeground="#fff",
+                          command=lambda: MyEntry7.insert(END, "Manufacturing Industries"))
+    manufacturingindustries_btn.pack(fill=X, padx = 50,pady= 5)
+
+    # update the canvas scroll region
+    inner_frame.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
     # update the button to close the tab
     MyButton3.config(command=Close_tab)
 
@@ -185,5 +324,13 @@ MyButton2.place(relx=0.935, rely=0.36, relwidth=0.04, relheight=0.036)
 MyButton3 = Button(image=my_img, bg="#fff", bd=0, activebackground="#fff", activeforeground="white",
 command=onClick_Intended)
 MyButton3.place(relx=0.425, rely=0.74, relwidth=0.04, relheight=0.036)
+
+MyButton4 = Button(window, text="Save",bg="#fff", activebackground="#fff", activeforeground="white",command=Save_Data)
+MyButton4.place(relx= 0.35,rely= 0.9, relwidth= 0.1, relheight= 0.05)
+
+MyButton5 = Button(window, text="Clear",bg="#fff", activebackground="#fff", activeforeground="white",command=Clear_Data)
+MyButton5.place(relx= 0.50,rely= 0.9, relwidth= 0.1, relheight= 0.05)
+
+
 
 window.mainloop()
