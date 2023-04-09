80de3656798df9a0430c0dfcbae0f1e334f4c13a
TatcataiTTN <truongtuannghia1248@gmail.com>
2023-04-09 21:07:49 +0700

    Change .csv to .xlsx

--- Database-and-BillPayment/Payment.py
+++ Database-and-BillPayment/Payment.py
@@ -1,4 +1,6 @@
-import csv
+import openpyxl
+import os
+import datetime
 class Payment:
     '''The Payment class includes the following methods:
 search_payment(PaymentID): Searches for a payment record by its ID.
@@ -6,60 +8,57 @@ search_payment_by_customer_id(CustomerID): Searches for payment records by a cus
 create_payment(): Creates a new payment record and appends it to the Payments.csv file.
 The user inputs the CustomerID, BillingID, Payment Date, and Payment Amount.
 The method checks if the BillingID and CustomerID match and if the Payment Amount matches the TotalBill in the Billing.csv file.'''
-    def __init__(self, PaymentID, BillingID, CustomerID, PaymentDate, Month, Year, PaymentAmount):
-        self.PaymentID = PaymentID
-        self.BillingID = BillingID
-        self.CustomerID = CustomerID
-        self.PaymentDate = PaymentDate
-        self.Month = Month
-        self.Year = Year
-        self.PaymentAmount = PaymentAmount
 
-    @staticmethod
-    def search_payment(PaymentID):
-        with open('Payments.csv', 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if row[0] == str(PaymentID):
-                    return row
-        return None
+    def __init__(self, payment_file, billing_file):
+        self.payment_file = payment_file
+        self.billing_file = billing_file
 
-    @staticmethod
-    def search_payment_by_customer_id(CustomerID):
-        results = []
-        with open('Payments.csv', 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if row[2] == str(CustomerID):
-                    results.append(row)
-        return results
+        if not os.path.exists(self.payment_file):
+            wb = openpyxl.Workbook()
+            ws = wb.active
+            ws.append(["PaymentID", "BillingID", "CustomerID", "PaymentDate", "Month", "Year", "PaymentAmount"])
+            wb.save(self.payment_file)
 
-    @staticmethod
-    def create_payment():
-        CustomerID = input("Enter the CustomerID: ")
-        BillingID = input("Enter the BillingID: ")
+    def search_payment(self, payment_id):
+        wb = openpyxl.load_workbook(self.payment_file)
+        ws = wb.active
+        data = []
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            if row[0] == payment_id:
+                data.append(row)
+        return data
 
-        # Check if the BillingID and CustomerID match
-        billing_data = Billing.search_billing(BillingID)
-        if not billing_data or billing_data[1] != CustomerID:
-            print("BillingID and CustomerID do not match.")
+    def search_payment_by_customer_id(self, customer_id):
+        wb = openpyxl.load_workbook(self.payment_file)
+        ws = wb.active
+        data = []
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            if row[2] == customer_id:
+                data.append(row)
+        return data
+
+    def create_payment(self, customer_id, billing_id, payment_date, month, year, payment_amount):
+        wb_billing = openpyxl.load_workbook(self.billing_file)
+        ws_billing = wb_billing.active
+
+        valid_billing = False
+        for row in ws_billing.iter_rows(min_row=2, values_only=True):
+            if row[0] == billing_id and row[1] == customer_id:
+                total_bill = row[8]
+                valid_billing = True
+                break
+
+        if not valid_billing:
+            print("Billing ID and Customer ID do not match. Cannot create payment.")
             return
 
-        # Get the TotalBill amount from the Billing.csv
-        total_bill = float(billing_data[9])
-
-        # Input the payment data
-        PaymentDate = input("Enter the Payment Date (dd-mm-yyyy): ")
-        PaymentMonth, PaymentYear = int(PaymentDate.split('-')[1]), int(PaymentDate.split('-')[2])
-        PaymentAmount = float(input("Enter the Payment Amount: "))
-
-        if PaymentAmount != total_bill:
-            print("Payment amount does not match the TotalBill.")
+        if payment_amount != total_bill:
+            print("Payment amount does not match the total bill. Cannot create payment.")
             return
 
-        # Append the payment data to the CSV file
-        with open('Payments.csv', 'a') as f:
-            writer = csv.writer(f)
-            PaymentID = sum(1 for row in csv.reader(open('Payments.csv')))  # Auto-increment PaymentID
-            payment_data = [PaymentID, BillingID, CustomerID, PaymentDate, PaymentMonth, PaymentYear, PaymentAmount]
-            writer.writerow(payment_data)
+        wb_payment = openpyxl.load_workbook(self.payment_file)
+        ws_payment = wb_payment.active
+        payment_id = ws_payment.max_row - 1
+        new_entry = [payment_id, billing_id, customer_id, payment_date, month, year, payment_amount]
+        ws_payment.append(new_entry)
+        wb_payment.save(self.payment_file)
