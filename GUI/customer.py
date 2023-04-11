01761a6de7fbeb511af8d751a0f2d23fb7cd6285
Blinh311 <banglinh.vu2003@gmail.com>
2023-04-12 00:10:19 +0700

    customer

--- GUI/customer.py
+++ GUI/customer.py
@@ -2,13 +2,17 @@ import tkinter as tk
 import openpyxl
 from tkinter import ttk
 import os
+from assets.set_logo import logo
 
 root = tk.Tk()
+icon = logo(root)
+root.iconphoto(False, icon)
 root.title('Customer list')
 root.option_add("*tearOff", False)
 root.pack_propagate(False)
 root.geometry("1280x720")
 root.resizable(0, 0)
+
 # Create a style
 style = ttk.Style(root)
 # Import the tcl file
