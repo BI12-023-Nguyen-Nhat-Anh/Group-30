5d079b1d13280db7ed67213416b5280659ac26f8
TatcataiTTN <truongtuannghia1248@gmail.com>
2023-04-17 17:37:55 +0700

    Alternative with Billing and ChartBilling

--- GUI/GUI-Billing.py
+++ GUI/GUI-Billing.py
@@ -1,32 +1,25 @@
 import tkinter as tk
-from tkinter import ttk
-from assets.set_logo import logo
 import openpyxl
-from Database import Billing
+from tkinter import ttk
+
+# Initialize root window
 root = tk.Tk()
-logo(root)
-root.title('Billing list')
-root.option_add("*tearOff", False)
+root.title('Billing Information')
 root.pack_propagate(False)
 root.geometry("1280x720")
 root.resizable(0, 0)
 
-# Create a style
+# Configure theme
 style = ttk.Style(root)
-# Import the tcl file
 theme_path = "assets/forest-dark.tcl"
-# Load the theme file
-root.tk.call("source", "forest-dark.tcl")
-
-# Set the theme with the theme_use method
+root.tk.call("source", theme_path)
 style.theme_use("forest-dark")
-
 
 """
 =================TREEVIEW======================================
 ===============================================================
 """
-# Panedwindow
+# Create paned window
 paned = ttk.PanedWindow(root)
 paned.grid(row=0, column=0, columnspan=10, padx=(10, 20),
            pady=(10, 10), sticky="nsew")
@@ -36,102 +29,178 @@ paned.pack_propagate(False)
 pane_1 = ttk.Frame(paned)
 paned.add(pane_1, weight=1)
 
-
 # tree frame
 treeFrame = ttk.Frame(pane_1)
 treeFrame.grid(row=0, column=0, padx=5, pady=5)
-
 
 # scroll ball
 treeScrolly = ttk.Scrollbar(treeFrame)
 treeScrolly.pack(side="right", fill="y")
 
 # contents
-cols = ("BillingID", "CustomerID", "ConsumptionID", "BillingDeadline", "Month", "Year", "BillingAmount","LateFee", "TotalBill", "Status")
+cols = ("BillingID", "CustomerID", "ConsumptionID",
+        "BillingDeadline", "BillingAmount", "LateFee", "TotalBill", "Status")
+treeview = ttk.Treeview(treeFrame, show="headings",
+                        yscrollcommand=treeScrolly.set, columns=cols, height=13)
 
-treeview = ttk.Treeview(treeFrame, show="headings",yscrollcommand=treeScrolly.set, columns=cols, height=13)
-
-treeview.column("BillingID", width=120, anchor="center")
-treeview.column("CustomerID", width=120, anchor="center")
-treeview.column("ConsumptionID", width=120, anchor="center")
-treeview.column("BillingDeadline", width=120, anchor="center")
-treeview.column("Month", width=120, anchor="center")
-treeview.column("Year", width=120, anchor="center")
-treeview.column("BillingAmount", width=120, anchor="center")
-treeview.column("LateFee", width=120, anchor="center")
-treeview.column("TotalBill", width=120, anchor="center")
-treeview.column("Status", width=120, anchor="center")
-treeview.bind('<Button-1>', lambda event: on_treeview_select(event))
-
+for col in cols:
+    treeview.heading(col, text=col)
+    treeview.column(col, width=140, anchor="center")
 treeview.pack(fill="both", expand=True)
 treeScrolly.config(command=treeview.yview)
 
-path = "data/data_customer.xlsx"
+path = "data/Billing.xlsx"
+workbook = openpyxl.load_workbook(path)
+sheet = workbook.active
 
-def search_data():
-    billing_id = billing_id_entry.get()
-    customer_id = customer_id_entry.get()
-
-    if billing_id != "BillingID":
-        results = billing.search_billing(billing_id)
-    elif customer_id != "CustomerID":
-        results = billing.search_billing_by_customer_id(customer_id)
-    else:
-        results = []
-
-    reset_treeview()
-
-    for row in results:
-        treeview.insert("", "end", values=row)
-
-
-def reset_treeview():
-    for row in treeview.get_children():
-        treeview.delete(row)
-
-    for row in billing.search_billing_by_customer_id("*"):
-        treeview.insert("", "end", values=row)
-
-
-def on_treeview_select(event):
-    pass  # Add any action to be performed when selecting a row in the treeview
-
-billing = Billing("Billing.xlsx", "Consumption.xlsx", "Customer.xlsx")
+# Load data into treeview
+def load_data():
+    global list_values
+    list_values = list(sheet.values)[1:]
+    for value_tuple in list_values:
+        treeview.insert("", tk.END, values=value_tuple[:8])
 
 """
 =================Search and filter======================================
 ===============================================================
 """
-# Search & filter
-search_frame = ttk.LabelFrame(root, text="Search and filter", padding=(20, 10))
-search_frame.grid(row=1, column=0, padx=(15, 10),pady=(10, 10), columnspan=2, sticky="nsew")
+# Search and filter
+search_frame = ttk.LabelFrame(
+    root, text="Search and filter", padding=(20, 10))
+search_frame.grid(row=1, column=0, padx=(15, 10),
+                  pady=(10, 10), columnspan=2, sticky="nsew")
 search_frame.columnconfigure(index=0, weight=1)
 search_frame.pack_propagate(False)
 
-# BillingID
+# CustomerID entry
+customer_id_entry = ttk.Entry(search_frame)
+customer_id_entry.insert(0, "CustomerID")
+# double left-click on entry and the default text disappears, right-click to get the default text
+customer_id_entry.bind("<Double-Button-1>", lambda e: customer_id_entry.delete(0, "end"))
+customer_id_entry.bind("<Button-3>", lambda e: customer_id_entry.insert(
+    0, "Customer Code") if not customer_id_entry.get() else None)
+customer_id_entry.grid(row=1, column=0, columnspan=2, pady=10, sticky="ew")
+
+# BillingID entry
 billing_id_entry = ttk.Entry(search_frame)
 billing_id_entry.insert(0, "BillingID")
 billing_id_entry.bind("<Double-Button-1>", lambda e: billing_id_entry.delete(0, "end"))
-billing_id_entry.bind("<Button-3>", lambda e: billing_id_entry.insert(0, "BillingID") if not billing_id_entry.get() else None)
-billing_id_entry.grid(row=1, column=0, columnspan=2, padx=(5, 5), pady=(5, 5))
+billing_id_entry.bind("<Button-3>", lambda e: billing_id_entry.insert(
+    0, "Name") if not billing_id_entry.get() else None)
+billing_id_entry.grid(row=2, column=0, columnspan=2, pady=10, sticky="ew")
 
-# CustomerID
-customer_id_entry = ttk.Entry(search_frame)
-customer_id_entry.insert(0, "CustomerID")
-customer_id_entry.bind("<Double-Button-1>", lambda e: customer_id_entry.delete(0, "end"))
-customer_id_entry.bind("<Button-3>", lambda e: customer_id_entry.insert(0, "CustomerID") if not customer_id_entry.get() else None)
-customer_id_entry.grid(row=1, column=2, columnspan=2, padx=(5, 5), pady=(5, 5))
 
-# Search Button
-search_button = ttk.Button(search_frame, text="Search", command=search_data)
-search_button.grid(row=1, column=4, padx=(5, 5), pady=(5, 5))
+# Status
+status_list = ["Pending", "Paid", "Overdue"]
+status_combo = ttk.Combobox(search_frame, state="readonly", values=status_list)
+status_combo.grid(row=4, column=0, pady=10, sticky="ew")
+# right-click to clear the option
+status_combo.bind("<Button-3>", lambda event: clear_selection_type(event))
 
-# Reset Button
-reset_button = ttk.Button(search_frame, text="Reset", command=reset_treeview)
-reset_button.grid(row=1, column=5, padx=(5, 5), pady=(5, 5))
+def clear_selection_type(event):
+    type_combo.selection_clear()
+    type_combo.set("Status")
+# Reset button
+reset_button = ttk.Button(search_frame, text="Reset", command=lambda: reset())
+reset_button.grid(row=5, column=1, pady=15, sticky="nsew")
+reset_button.bind("<Button-1>", lambda event: reset(event))
 
-# Populate the treeview
-reset_treeview()
+# Search button
+search_button = ttk.Button(search_frame, text="Search", style="Accent.TButton", command=lambda: search())
+search_button.grid(row=5, column=0, pady=15, padx=(0, 10), sticky="nsew")
+search_button.bind("<Button-1>", lambda event: search(event))
 
+def search(event):
+    # Get the values from the search inputs
+    customer_id_value = customer_id_entry.get()
+    billing_id_value = billing_id_entry.get()
+    status_value = status_combo.get()
+
+    # Clear the treeview
+    treeview.delete(*treeview.get_children())
+
+    # Filter the data based on the search inputs
+    filtered_data = []
+    for value_tuple in list_values:
+        if ((customer_id_value == "CustomerID" or customer_id_value in value_tuple[1]) and
+            (billing_id_value == "BillingID" or billing_id_value in value_tuple[0])):
+            if status_value == "Status":
+                filtered_data.append(value_tuple[:8])
+            elif status_value in value_tuple[7]:
+                filtered_data.append(value_tuple[:8])
+
+    # Update the treeview with the filtered data
+    for value_tuple in filtered_data:
+        treeview.insert("", tk.END, values=value_tuple)
+
+def reset(event):
+    customer_id_entry.delete(0, "end")
+    customer_id_entry.insert(0, "CustomerID")
+    billing_id_entry.delete(0, "end")
+    billing_id_entry.insert(0, "BillingID")
+    status_combo.set("Status")
+    treeview.delete(*treeview.get_children())
+
+    # ensure a row of headers is not added in the treeview
+    for value_tuple in list_values:
+        treeview.insert("", tk.END, values=value_tuple[:8])
+
+
+"""
+=================Notebook======================================
+===============================================================
+"""
+
+paned2 = ttk.PanedWindow(root)
+paned2.grid(row=1, column=2, padx=(10, 0),
+            pady=(18, 0), columnspan=7, sticky="nsew")
+# Pane #2
+pane_2 = ttk.Frame(paned)
+paned.add(pane_2, weight=2)
+paned2.pack_propagate(False)
+
+# Notebook
+notebook = ttk.Notebook(pane_2, height=280)
+notebook.pack(fill=tk.BOTH, expand=True)
+
+# Tab #1
+tab_1 = ttk.Frame(notebook)
+notebook.add(tab_1, text="Details")
+
+def on_treeview_select(event):
+    selection = event.widget.selection()
+    if selection:
+        selected_item = treeview.focus()
+        chosen_id = treeview.item(selected_item)['values'][0]
+        path = "data/Billing.xlsx"
+        workbook = openpyxl.load_workbook(path)
+        sheet = workbook.active
+        cols = ("BillingID", "CustomerID", "ConsumptionID", "BillingDeadline", "BillingAmount", "LateFee", "TotalBill", "Status")
+        row = None
+        for r in sheet.iter_rows(min_row=2):
+            if r[0].value == chosen_id:
+                row = r
+                break
+
+        if row:
+            for widget in tab_1.winfo_children():
+                widget.destroy()
+
+            for j, header in enumerate(cols):
+                header = cols[j]
+                ttk.Label(tab_1, text=header + ":").grid(
+                    column=0, row=j, sticky="w", pady=5, padx=10)
+                ttk.Label(tab_1, text=row[j].value).grid(
+                    column=1, row=j, sticky="w", pady=5, padx=10)
+
+treeview.bind('<<TreeviewSelect>>', on_treeview_select)
+
+# center window
+root.update()
+root.minsize(root.winfo_width(), root.winfo_height())
+x_cordinate = int((root.winfo_screenwidth()/2) - (root.winfo_width()/2))
+y_cordinate = int((root.winfo_screenheight()/2) - (root.winfo_height()/2))
+root.geometry("+{}+{}".format(x_cordinate, y_cordinate))
+
+load_data()
 root.mainloop()
-
---
+++ GUI/GUI-ChartBilling.py
@@ -0,0 +1,195 @@
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from tkinter import ttk
+import openpyxl
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+
+# Initialize root window
+root = tk.Tk()
+root.title("Billing Chart")
+root.geometry("1280x960")
+root.resizable(False, False)
+root.withdraw()  # Hide root window initially
+
+# Configure custom theme
+style = ttk.Style(root)
+theme_path = "assets/forest-dark.tcl"
+root.tk.call("source", theme_path)
+style.theme_use("forest-dark")
+
+# Load customer data from Excel file
+def load_data():
+    file_path = "data/Billing.xlsx"
+    df = pd.read_excel(file_path)
+    return df
+
+data = load_data()
+
+filtered_data = None
+
+# Get Customer ID
+def get_customer_id():
+    global customer_id
+    customer_id_win = tk.Toplevel(root)
+    customer_id_win.title("Enter Customer ID")
+    customer_id_win.resizable(False, False)
+
+    ttk.Label(customer_id_win, text="Enter Customer ID:").pack(padx=5, pady=(5, 0), anchor="w")
+    customer_id_entry = ttk.Entry(customer_id_win)
+    customer_id_entry.pack(padx=5, pady=(0, 5), anchor="w")
+
+    def submit():
+        global customer_id
+        customer_id = customer_id_entry.get()
+
+        if customer_id not in data["CustomerID"].unique():
+            messagebox.showerror("Error", "Customer ID not found.")
+        else:
+            customer_id_win.destroy()
+            main_app()
+
+    ttk.Button(customer_id_win, text="Submit", command=submit).pack(padx=5, pady=5, anchor="w")
+    customer_id_win.mainloop()
+
+# Main application
+def main_app():
+    root.deiconify()
+    global filtered_data
+    # Filter data based on customer ID
+    filtered_data = data[data["CustomerID"] == customer_id].sort_values(["Year", "Month"])
+
+    # Create paned window
+    paned_window = ttk.PanedWindow(root, orient=tk.VERTICAL)
+    paned_window.pack(fill=tk.BOTH, expand=True)
+
+    chart_pane = ttk.Frame(paned_window, width=1200, height=600)
+    paned_window.add(chart_pane)
+
+    details_pane = ttk.Frame(paned_window, width=1200, height=150)
+    paned_window.add(details_pane)
+
+    export_pane = ttk.Frame(paned_window, width=1200, height=50)
+    paned_window.add(export_pane)
+
+    # Chart type variable
+    chart_type = tk.StringVar()
+    chart_type.set("BillingAmount")
+
+    # Plot chart function
+    def plot_chart():
+        # Clear existing chart
+        ax.clear()
+
+        # Modified function to generate command for checkbuttons
+        def create_toggle_line_visibility_fn(checkbutton_var, line):
+            def toggle_line_visibility():
+                line.set_visible(checkbutton_var.get())
+                canvas.draw()
+
+            return toggle_line_visibility
+
+        lines = []  # Store line objects for the custom legend
+        for year in filtered_data["Year"].unique()[-3:]:
+            year_data = filtered_data[filtered_data["Year"] == year]
+            months = year_data["Month"].tolist()
+            values = year_data[chart_type.get()].tolist()
+            line, = ax.plot(months, values, label=str(year), marker='o', picker=5)  # Added ',' after line to unpack the tuple
+            lines.append(line)
+
+        # Create custom legend with checkbuttons
+        legend_items = []
+        for line in lines:
+            checkbutton_var = tk.BooleanVar()
+            checkbutton_var.set(True)
+            toggle_fn = create_toggle_line_visibility_fn(checkbutton_var, line)  # Generate the command function
+            checkbutton = ttk.Checkbutton(canvas.get_tk_widget(), text=line.get_label(), variable=checkbutton_var,  # Use canvas.get_tk_widget()
+                                        command=toggle_fn)  # Use the generated function as command
+            legend_items.append((line, [checkbutton]))
+
+        legend = ax.legend(handles=[item[0] for item in legend_items], loc='upper left', bbox_to_anchor=(1, 1))
+
+        # Place checkbuttons in the correct position
+        for idx, (_, widgets) in enumerate(legend_items):
+            widget = widgets[0]
+            x = (legend.get_window_extent(renderer=canvas.get_renderer()).xmax + 200)  # Use the legend's window extent to calculate the x position
+            y_offset = 20  # Adjust the vertical distance between checkbuttons
+            y = legend.get_window_extent(renderer=canvas.get_renderer()).ymin + idx * y_offset  # Calculate the y position for each checkbutton based on the legend's ymin and the y_offset
+            widget.place(x=x, y=y)  # Place the checkbuttons in the canvas widget
+
+
+        # Customize chart
+        ax.set_xlabel("Month")
+        ax.set_ylabel(chart_type.get())
+        ax.set_title(f"{chart_type.get()} Chart")
+        ax.set_xticks(range(1, 13))
+        if chart_type.get() == "BillingAmount":
+            ax.set_ylim(0, max(filtered_data["BillingAmount"]) * 1.2)
+        else:
+            ax.set_ylim(0, max(filtered_data["TotalBill"]) * 1.2)
+        ax.grid()
+        canvas.draw()
+
+
+    # Create chart
+    fig = Figure(figsize=(10, 6), dpi=100)
+    ax = fig.add_subplot(111)  # Changed variable name from plt to ax
+    canvas = FigureCanvasTkAgg(fig, master=chart_pane)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    # Create a function to handle clicks on chart data points
+    def on_pick(event):
+        ind = event.ind[0]
+        selected_data = filtered_data.iloc[ind]
+        details_tab_text.delete(1.0, tk.END)
+        details = f"BillingID: {selected_data['BillingID']}\n" \
+                  f"CustomerID: {selected_data['CustomerID']}\n" \
+                  f"ConsumptionID: {selected_data['ConsumptionID']}\n" \
+                  f"BillingDeadline: {selected_data['BillingDeadline']}\n" \
+                  f"BillingAmount: {selected_data['BillingAmount']}\n" \
+                  f"LateFee: {selected_data['LateFee']}\n" \
+                  f"TotalBill: {selected_data['TotalBill']}\n" \
+                  f"Status: {selected_data['Status']}"
+        details_tab_text.insert(tk.END, details)
+
+    # Connect the pick function to the chart
+    canvas.mpl_connect('pick_event', on_pick)
+
+    # Chart type combobox
+    chart_type_combobox = ttk.Combobox(details_pane, textvariable=chart_type, state="readonly", values=["BillingAmount", "TotalBill"])
+    chart_type_combobox.pack(padx=5, pady=5, anchor="w")
+    chart_type_combobox.bind("<<ComboboxSelected>>", lambda _: plot_chart())
+
+    # Details tab
+    notebook = ttk.Notebook(details_pane)
+    details_tab = ttk.Frame(notebook)
+    notebook.add(details_tab, text="Details")
+    notebook.pack(fill=tk.BOTH, expand=True)
+
+    details_tab_text = tk.Text(details_tab, wrap=tk.WORD)
+    details_tab_text.pack(fill=tk.BOTH, expand=True)
+
+    # Export chart to png
+    def export_chart():
+        file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+        if file_path:
+            fig.savefig(file_path)
+
+    export_button = ttk.Button(export_pane, text="Export Chart", command=export_chart)
+    export_button.pack(padx=5, pady=5, anchor="w")
+
+    # Plot initial chart
+    plot_chart()
+
+    # Center the window on the screen and start the tkinter main event loop
+    root.update_idletasks()
+    x = (root.winfo_screenwidth() - root.winfo_reqwidth()) // 2
+    y = (root.winfo_screenheight() - root.winfo_reqheight()) // 2
+    root.geometry(f"+{x}+{y}")
+    root.mainloop()
+
+get_customer_id()
