84944d8f3c62c180d4be0afad8aba2771e1ea779
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-04-17 23:12:13 +0700

    MeterReading

--- MeterReading.py
+++ MeterReading.py
@@ -1,58 +1,61 @@
 from main import list_user
 from GUI.login import phone, password
-from openpyxl.utils.dataframe import dataframe_to_rows
-import openpyxl
 import random
 import pandas as pd
 from tkinter import messagebox
 
 class MeterReading:
     def __init__(self):
-        # write=pd.ExcelWriter("data/data_meterreading.xlsx", engine="openpyxl")
+        global amount
+        amount=0
+        # Check with which account the user is logged in to register electricity
         for custom in list_user:
             if(custom.get_phone()==phone):
                 if(custom.get_password()==password):
                     self.__customer_id=int(custom.get_id_card())
-                    print(f"Customer id={self.__customer_id}")
+                    # Read data_customer to get all information of that account
                     data=pd.read_excel("data/data_customer.xlsx",sheet_name="filtered_data")
                     data_meterreading=pd.read_excel("data/data_meterreading.xlsx",sheet_name="MeterReading")
-                    # write=pd.ExcelWriter("data/data.xlsx",engine="openpyxl")
+                    # Take status of that account to check
                     status=data.loc[data["Identity number"]==self.__customer_id,"Status"].values[0]
+                    print(f"Status: {status}")
                     if(status=="Active"):
+                        # Get the data in which line has the same Customer_id as the logged in account
                         name=data.loc[data["Identity number"]==self.__customer_id,"Name"].values[0]
                         id_customer=data.loc[data["Identity number"]==self.__customer_id,"Customer Code"].values[0]
+                        # Give id_customer = id_customer to have the account check variable in the get_MeterReading function because meter reading 
+                        # only takes the customer code
                         self.__customer_code=id_customer
-                        type=data.loc[data["Identity number"]==self.__customer_id,"Type"].values[0]
+                        types=data.loc[data["Identity number"]==self.__customer_id,"Type"].values[0]
                     else:
+                        # # If customers pay late fees, they will be inactive and unable to use electricity
                         messagebox.showerror("Error!","Your account is invalid due to late payment of fees")
-
-                    new_data={"Customer Code": id_customer, "Name": name, "MeterReading": "","Type": type, "Status": status}
+                    # Check the types of electricity customers use and random amount of electricity
+                    if(types=="Household"):
+                        amount=random.randint(50,400)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                    elif(types=="Manufacturing industries"):
+                        amount=random.randint(400,1000)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                    elif(types=="Business"):
+                        amount=random.randint(200,700)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                    elif(types=="Administrative offices"):
+                        amount=random.randint(200,700)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                    # Write data to excel file
+                    new_data={"Customer Code": id_customer, "Name": name, "MeterReading": amount, "Type": types, "Status": status}
                     df=pd.DataFrame(new_data,index=[0])
                     update=pd.concat([data_meterreading,df])
                     update.to_excel("data/data_meterreading.xlsx", index=False, sheet_name="MeterReading")
-
-    def set_MeterReading(self):
-        # Random amount of electricity used by the customer 
-        data=pd.read_excel("data/data_meterreading.xlsx",sheet_name="MeterReading")
-        type=data.loc[data["Customer Code"]==self.__customer_code, "Type"].values[0]
-        if(type=="Household"):
-            data.loc[data["Customer Code"]==self.__customer_code, "MeterReading"]=random.randint(50,400)
-            data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
-        elif(type=="Manufacturing industries"):
-            data.loc[data["Customer Code"]==self.__customer_code, "MeterReading"]=random.randint(400,1000)
-            data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
-        elif(type=="Business"):
-            data.loc[data["Customer Code"]==self.__customer_code, "MeterReading"]=random.randint(200,700)
-            data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
-        elif(type=="Administrative offices"):
-            data.loc[data["Customer Code"]==self.__customer_code, "MeterReading"]=random.randint(200,700)
-            data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+        
+    def get_customer_code(self):
+        return self.__customer_code
 
     def get_MeterReading(self):
-        # Take the data by the CustomerID in the data_customer file
-        data=pd.read_excel("data/data_customer.xlsx",sheet_name="filtered_data")
-        user_data=data.loc[data["Customer Code"]==self.__self.__customer_id, "MeterReading"].values[0]
-        
-
-k=MeterReading()
-k.set_MeterReading()
+        # Take the data by the Customer Code in the data_MeterReading file
+        data=pd.read_excel("data/data_meterreading.xlsx",sheet_name="MeterReading")
+        global user_meterreading
+        user_meterreading=data.loc[data["Customer Code"]==self.__customer_code, "MeterReading"].values[0]
+        return user_meterreading
+MeterReading()
