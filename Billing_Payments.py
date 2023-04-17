4c9c474743f88a5dd197c73b336c609bbb63a44e
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-04-17 22:59:07 +0700

    Billing Payment

--- Billing_Payments.py
+++ Billing_Payments.py
@@ -3,57 +3,91 @@ import random
 from domain.customer import customer
 global total
 total = 0
+global status
+status = ""
+global due
+due = ""
+global payment
+payment = ""
 # Create a dictionary to map the random number to the corresponding string value
-mapping = {1: "Intime", 2: "Out of date"}
 
-# Generate a random number between 1 and 2 (inclusive)
-random_number = random.randint(1, 2)
-random_number_1 = random.randint(1, 2)
-random_number_2 = random.randint(1, 2)
-random_number_3 = random.randint(1, 2)
+duedate = random.randint(5,19)
+payment_date = random.randint(18,30)
+duedate1 = random.randint(1,20)
+payment_date1 = random.randint(15,25)
+duedate2 = random.randint(20,30)
+payment_date2 = random.randint(1,25)
+duedate3 = random.randint(16,26)
+payment_date3 = random.randint(5,29)
 
 
 if customer.get_type() == "HouseHold":
     def Household_payment():
-        if random_number == 1:
+        if duedate >= payment_date:
             total = numbers[0]
+            status = "Paid"
+            due = f"{duedate}/3/2023"
+            payment = f"{payment_date}/3/2023"
         else:
             
             total = late_fee_HouseHold()
+            status = "Overdue"
+            due = f"{duedate}/3/2023"
+            payment = f"{payment_date}/3/2023"
             return total
 elif customer.get_type() == "Administrative_offices":
     def Administrative_offices_payment():
-        if random_number_1 == 1:
+        if duedate1 >= payment_date1:
             total = numbers1[0]
+            status = "Paid"
+            due = f"{duedate1}/3/2023"
+            payment = f"{payment_date1}/3/2023"
         else:
             
             total = late_fee_Administrative_offices()
+            status = "Overdue"
+            due = f"{duedate1}/3/2023"
+            payment = f"{payment_date1}/3/2023"
             return total
 elif customer.get_type() == "Business":
     def Business_payment():
-        if random_number_2 == 1:
+        if duedate2 >= payment_date2:
             total = numbers2[0]
-            
+            status = "Paid"
+            due = f"{duedate2}/3/2023"
+            payment = f"{payment_date2}/3/2023"
         else:
             
             total = late_fee_Business()
+            status = "Overdue"
+            due = f"{duedate2}/3/2023"
+            payment = f"{payment_date2}/3/2023"
             return total
 elif customer.get_type() == "Manufacturing_industries":
     def Manufacturing_industries_payment():
-        if random_number_3 == 1:
+        if duedate3 >= payment_date3:
             total = numbers3[0]
+            status = "Paid"
+            due = f"{duedate3}/3/2023"
+            payment = f"{payment_date3}/3/2023"
         else:
             
             total = late_fee_Manufacturing_industries()
+            status = "Overdue"
+            due = f"{duedate3}/3/2023"
+            payment = f"{payment_date3}/3/2023"
             return total
 else:
     print("Error!")
 
-
-Household_payment()
-Administrative_offices_payment()
-Business_payment()
-Manufacturing_industries_payment()
+if customer.get_type() == "HouseHold":
+    Household_payment()
+elif customer.get_type() == "Administrative_offices":
+    Administrative_offices_payment()
+elif customer.get_type() == "Business":
+    Business_payment()
+elif customer.get_type() == "Manufacturing_industries":
+    Manufacturing_industries_payment()
 
 
 
