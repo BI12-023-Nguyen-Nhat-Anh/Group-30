ae5a8134a47fb14817536cbfe8a749aebcdc9989
nanh123 <anhnn.bi12-023@st.usth.edu.vn>
2023-04-18 00:08:02 +0700

    Data

--- domain/MeterReading.py
+++ domain/MeterReading.py
@@ -15,7 +15,7 @@ class MeterReading:
                     self.__customer_id=int(custom.get_id_card())
                     # Read data_customer to get all information of that account
                     data=pd.read_excel("data/data_customer.xlsx",sheet_name="filtered_data")
-                    data_meterreading=pd.read_excel("data/data_meterreading.xlsx",sheet_name="MeterReading")
+                    data_meterreading=pd.read_excel("data/data_meterreading.xlsx",sheet_name="Total Consumtion")
                     # Take status of that account to check
                     status=data.loc[data["Identity number"]==self.__customer_id,"Status"].values[0]
                     print(f"Status: {status}")
@@ -33,23 +33,24 @@ class MeterReading:
                     # Check the types of electricity customers use and random amount of electricity
                     if(types=="Household"):
                         amount=random.randint(50,400)
-                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="Total Consumtion",index=False)
                     elif(types=="Manufacturing industries"):
                         amount=random.randint(400,1000)
-                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="Total Consumtion",index=False)
                     elif(types=="Business"):
                         amount=random.randint(200,700)
-                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="Total Consumtion",index=False)
                     elif(types=="Administrative offices"):
                         amount=random.randint(200,700)
-                        data.to_excel("data/data_meterreading.xlsx", sheet_name="MeterReading",index=False)
+                        data.to_excel("data/data_meterreading.xlsx", sheet_name="Total Consumtion",index=False)
                     # Write data to excel file
                     new_data={"Customer Code": id_customer, "Name": name, "MeterReading": amount, "Type": types, "Status": status}
                     df=pd.DataFrame(new_data,index=[0])
                     update=pd.concat([data_meterreading,df])
-                    update.to_excel("data/data_meterreading.xlsx", index=False, sheet_name="MeterReading")
+                    update.to_excel("data/data_meterreading.xlsx", index=False, sheet_name="Total Consumtion")
         
     def get_customer_code(self):
         return self.__customer_code
 
 MeterReading()
+import Bill_Caculate
